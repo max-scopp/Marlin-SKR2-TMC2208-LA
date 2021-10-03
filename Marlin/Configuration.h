f278aa8bf8e211a6c916b985ad98be031cfac144
Max Scopp <me@maxscopp.de>
2021-10-03 22:30:49 +0200

    Define proper print area

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1362,16 +1362,16 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 350
+#define Y_BED_SIZE 350
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
-#define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define X_MIN_POS  0
+#define Y_MIN_POS  0
+#define Z_MIN_POS  0
+#define X_MAX_POS  X_BED_SIZE
+#define Y_MAX_POS  Y_BED_SIZE
+#define Z_MAX_POS  400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
