bc573c26158c417369dfb372e0f2fdcade915820
Max Scopp <me@maxscopp.de>
2021-10-03 22:29:16 +0200

    LCD allow western symbols like umlauts

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2073,7 +2073,7 @@
  *
  * :['JAPANESE', 'WESTERN', 'CYRILLIC']
  */
-#define DISPLAY_CHARSET_HD44780 JAPANESE
+#define DISPLAY_CHARSET_HD44780 WESTERN
 
 /**
  * Info Screen Style (0:Classic, 1:Průša)
