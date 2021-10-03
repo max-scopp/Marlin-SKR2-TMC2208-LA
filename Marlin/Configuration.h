1e96d19607404267f6bfcb0118e167a3cb131ab8
Max Scopp <me@maxscopp.de>
2021-10-04 01:17:26 +0200

    Fix PLA preset fan speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1863,7 +1863,7 @@
 #define PREHEAT_1_TEMP_HOTEND 185
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_TEMP_CHAMBER 35
-#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_1_FAN_SPEED    255 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 245
