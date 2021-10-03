e68f7325d72d1e25f4942cfe3cc43b90f2ca42d1
Max Scopp <me@maxscopp.de>
2021-10-03 22:30:32 +0200

    Fix homing directions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1352,8 +1352,8 @@
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
-#define X_HOME_DIR -1
-#define Y_HOME_DIR -1
+#define X_HOME_DIR 1
+#define Y_HOME_DIR 1
 #define Z_HOME_DIR -1
 //#define I_HOME_DIR -1
 //#define J_HOME_DIR -1
